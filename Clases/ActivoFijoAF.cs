f3119ebc292bcd45254f325ba75246e8d651bed0
KevinJovel <kevinjovel9@gmail.com>
2020-05-31 12:41:30 -0600

    metodo para listar los no asignados

---
+++ Clases/ActivoFijoAF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASGARDAPI.Clases
+{
+    public class ActivoFijoAF
+    {
+        public int IdBien { get; set; }
+        public int NoFormulario { get; set; }
+        public DateTime? FechaIngreso { get; set; }
+        public string Desripcion { get; set; }
+        public string Clasificacion { get; set; }
+        public string Marca { get; set; }
+    }
+}
---
+++ Controllers/ActivoFijoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASGARDAPI.Models;
+using ASGARDAPI.Clases;
+
+namespace ASGARDAPI.Controllers
+{
+    public class ActivoFijoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("api/ActivoFIjo/listarActivosNoAsignados")]
+        public IEnumerable<ActivoFijoAF> listarActivosNoAsignados()
+        {
+            using (BDAcaassAFContext bd = new BDAcaassAFContext())
+            {
+                IEnumerable<ActivoFijoAF> lista = (from activo in bd.ActivoFijo
+                                                   join noFormulario in bd.FormularioIngreso
+                                                   on activo.NoFormulario equals noFormulario.NoFormulario
+                                                   join clasif in bd.Clasificacion
+                                                   on activo.IdClasificacion equals clasif.IdClasificacion
+                                                   join marca in bd.Marcas
+                                                   on activo.IdMarca equals  marca.IdMarca
+                                                   //where activo.EstadoActual == 1 && activo.EstaAsignado==0
+                                                   //orderby noFormulario.NoFormulario
+                                                           select new ActivoFijoAF
+                                                           {
+                                                               IdBien=activo.IdBien,
+                                                               NoFormulario=noFormulario.NoFormulario,
+                                                               FechaIngreso=noFormulario.FechaIngreso,
+                                                               Desripcion=activo.Desripcion,
+                                                               Clasificacion=clasif.Clasificacion1,
+                                                               Marca=marca.Marca
+                                                           }).ToList();
+                return lista;
+            }
+        }
+    }
+}
