6e1b6422332e77bdbfdce40b6a34eaeb4cdc8265
Saúl Reyes <saulr1016.sr3@gmail.com>
2020-06-01 18:26:41 -0600

    combos

---
+++ Clases/SolicitudMantenimientoAF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASGARDAPI.Clases
+{
+    public class SolicitudMantenimientoAF
+    {
+        public int idsolicitud { get; set; }
+        public DateTime? fechasolicitud { get; set; }
+        public string areasolicitante  { get; set; }
+        public string personasolicitante { get; set; }
+
+        //datos del modal
+
+        public int codigobien { get; set; }
+
+        public string descripcionbien { get; set; }
+        public string razonesmantenimiento { get; set; }
+        public string periodomantenimiento { get; set; }
+
+        //pk relacioandas
+        
+        public int idmantenimiento { get; set; }
+        public int idbien { get; set; } //tabla activo fijo
+        public int idresponsable { get; set; } //foranea en activo fijo, pk de empelado
+
+        public int areadenegocio { get; set; } //fk de empelado, pk de area de negocio
+        public int idsucursal { get; set; } //fk de area de negocio, pk de sucursal
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Controllers/SolicitudMantenimientoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASGARDAPI.Models;
+using ASGARDAPI.Clases;
+
+namespace ASGARDAPI.Controllers
+{
+    public class SolicitudMantenimientoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("api/SolicitudMantenimiento/listarEmpleadosCombo")]
+        public IEnumerable<EmpleadoAF> listarEmpleadosCombo()
+        {
+            using (BDAcaassAFContext bd = new BDAcaassAFContext())
+            {
+
+                IEnumerable<EmpleadoAF> lista = (from empleado in bd.Empleado
+                                                 where empleado.Dhabilitado == 1
+                                                 select new EmpleadoAF
+                                                 {
+                                                     idempleado = empleado.IdEmpleado,
+                                                     nombres = empleado.Nombres,
+                                                     apellidos = empleado.Apellidos
+                                                 }).ToList();
+                return lista;
+            }
+        }
+        [HttpGet]
+        [Route("api/SolicitudMantenimiento/listarAreaCombo")]
+        public IEnumerable<AreasDeNegocioAF> listarAreaCombo()
+        {
+            using (BDAcaassAFContext bd= new BDAcaassAFContext())
+            {
+                IEnumerable<AreasDeNegocioAF> lista = (from area in bd.AreaDeNegocio
+                                                       join sucursal in bd.Sucursal
+                                                       on area.IdSucursal equals sucursal.IdSucursal
+                                                       where area.Dhabilitado == 1
+                                                       select new AreasDeNegocioAF
+                                                       {
+                                                           IdAreaNegocio = area.IdAreaNegocio,
+                                                           Nombre = area.Nombre,
+                                                           nombreSucursal = sucursal.Nombre
+                                                       }).ToList();
+                return lista;
+            }
+
+        }
+    }
+}
