bed870a673ddfbdcbe1508ce85d5e460c3f38168
kevRei08 <kevin_rey09@hotmail.com>
2020-06-10 12:49:46 -0600

    clase para nuevo bien

---
+++ Clases/ActivoAF.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ASGARDAPI.Clases
+{
+    public class ActivoAF
+    {
+        //Variables de activo fijo
+        public int idbien { get; set; }
+        public int noformularioactivo { get; set; }
+        public string descripcion { get; set; }
+        public string modelo { get; set; }
+        public int tipoadquicicion { get; set; }
+        public string color { get; set; }
+        public int idmarca { get; set; }
+        public int idclasificacion { get; set; }
+        public int idproveedor { get; set; }
+        public int iddonante { get; set; }
+        public int idresponsable { get; set; } // Empleado
+        public string estadoingreso { get; set; }
+        public double valoradquicicion { get; set; }
+        public string plazopago { get; set; }
+        public double prima { get; set; }
+        public double cuotaasignada { get; set; }
+        public double interes { get; set; }
+        public double valorresidual { get; set; }
+        public string foto { get; set; }
+
+    }
+}
