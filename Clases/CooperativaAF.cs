4b43869294e66fd4f34a1a30985e520ee3968640
kevRei08 <kevin_rey09@hotmail.com>
2020-08-20 08:46:04 -0600

    Controller-Cooperativa

---
+++ Clases/CooperativaAF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ASGARDAPI.Clases
+{
+    public class CooperativaAF
+    {
+        //Propiedades
+        public int idcooperativa { get; set; }
+        public string nombre { get; set; }
+        public string logo { get; set; }
+        public string descripcion { get; set; }
+        public int anio { get; set; }
+        public int dhabilitado { get; set; }
+
+    }
+}
