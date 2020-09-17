90e94e19e441d3bc31d6a2a7de4dc8ffc0af44f2
Saúl Reyes <saulr1016.sr3@gmail.com>
2020-09-17 13:09:57 -0600

    historial de mantenimiento

--- Clases/InformeMatenimientoAF.cs
+++ Clases/InformeMatenimientoAF.cs
@@ -16,13 +16,20 @@ namespace ASGARDAPI.Clases
         public string fechacadena { get; set; }
         public DateTime? fechainforme { get; set; }
         public int idtecnico { get; set; }
-        public string nombretecnico { get; set; }
-        public string descripcion { get; set; }
+        public string nombretecnico { get; set; } 
         public double costomateriales { get; set; }
         public double costomo { get; set; }
         public double costototal { get; set; }
-        
+
+
+        //para los datos de arriba de la tabla:
         public string codigo { get; set; }
+        public string descripcion { get; set; }
+        public string encargado { get; set; }
+        public string areadenegocio { get; set; }
+
+
+
 
 
 
