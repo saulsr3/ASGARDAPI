4bea334b89b6832e76f97745859f8e29dabbd90b
kevRei08 <kevin_rey09@hotmail.com>
2020-10-26 12:56:53 -0600

    Clase activo modificar

---
+++ Clases/ActivoModificarAF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ASGARDAPI.Clases
+{
+    public class ActivoModificarAF
+    {
+        //Variables 
+        public int idbien { get; set; }
+        public int noformulario { get; set; }
+        public string fechaingreso { get; set; }
+        public string nofactura { get; set; }
+        public int tipoactivo { get; set; }
+        public string color { get; set; }
+        public string descripcion { get; set; }
+        public string modelo { get; set; }
+        public int tipoadquicicion { get; set; }
+        public int idmarca { get; set; }
+        public int idclasificacion { get; set; }
+
+    }
+}
