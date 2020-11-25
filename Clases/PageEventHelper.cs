82a93cc7d9d9cd3f0e2844d6b3f50c37a2c6b344
kevRei08 <kevin_rey09@hotmail.com>
2020-11-24 18:42:50 -0600

    Paginado y encabezado dinámico-Reportes

---
+++ Clases/PageEventHelper.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+namespace ASGARDAPI.Clases
+{
+    public class PageEventHelper : PdfPageEventHelper
+    {
+        PdfContentByte cb;
+        PdfTemplate template;
+        BaseFont bf = null;
+        DateTime PrintTime = DateTime.Now;
+        #region Properties
+
+        private string _Title;
+        public string Title
+        {
+            get { return _Title; }
+            set { _Title = value; }
+        }
+
+
+        private string _HeaderLeft;
+        public string HeaderLeft
+        {
+            get { return _HeaderLeft; }
+            set { _HeaderLeft = value; }
+        }
+
+        private string _HeaderRight;
+        public string HeaderRight
+        {
+            get { return _HeaderRight; }
+            set { _HeaderRight = value; }
+        }
+
+        private Font _HeaderFont;
+        public Font HeaderFont
+        {
+            get { return _HeaderFont; }
+            set { _HeaderFont = value; }
+        }
+
+        private Font _FooterFont;
+        public Font FooterFont
+        {
+            get { return FooterFont; }
+            set { FooterFont = value; }
+        }
+
+        #endregion
+
+
+        //Métodos
+        public override void OnOpenDocument(PdfWriter writer, Document document)
+        {
+            try
+            {
+                PrintTime = DateTime.Now;
+                bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                cb = writer.DirectContent;
+                template = cb.CreateTemplate(document.PageSize.Width, 50);
+            }
+            catch (DocumentException de)
+            {
+            }
+
+           // base.OnOpenDocument(writer, document);
+        }
+
+        public override void OnStartPage(PdfWriter writer, Document document)
+        {
+            base.OnStartPage(writer, document);
+            Rectangle pageSize = document.PageSize;
+
+            if(Title != string.Empty)
+            {
+                cb.BeginText();
+                cb.SetFontAndSize(bf, 15);
+                cb.SetRgbColorFill(50, 50, 200);
+                cb.SetTextMatrix(pageSize.GetLeft(40), pageSize.GetTop(40));
+                cb.ShowText(Title);
+                cb.EndText();
+            }
+            if (HeaderLeft + HeaderRight != string.Empty)
+            {
+                PdfPTable HeaderTable = new PdfPTable(2);
+                HeaderTable.DefaultCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                HeaderTable.TotalWidth = pageSize.Width - 80;
+                HeaderTable.SetWidthPercentage(new float[] { 45, 45 }, pageSize);
+
+                PdfPCell HeaderLeftCell = new PdfPCell(new Phrase(8, HeaderLeft, HeaderFont));
+                HeaderLeftCell.Padding = 5;
+                HeaderLeftCell.PaddingBottom = 8;
+                HeaderLeftCell.BorderWidthRight = 0;
+                HeaderTable.AddCell(HeaderLeftCell);
+                PdfPCell HeaderRightCell = new PdfPCell(new Phrase(8, HeaderRight, HeaderFont));
+                HeaderRightCell.HorizontalAlignment = PdfPCell.ALIGN_RIGHT;
+                HeaderRightCell.Padding = 5;
+                HeaderRightCell.PaddingBottom = 8;
+                HeaderRightCell.BorderWidthLeft = 0;
+                HeaderTable.AddCell(HeaderRightCell);
+                cb.SetRgbColorFill(0, 0, 0);
+                HeaderTable.WriteSelectedRows(0, -1, pageSize.GetLeft(40), pageSize.GetTop(50), cb);
+            }
+        }
+
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            base.OnEndPage(writer, document);
+            int PageN = writer.PageNumber;
+            String text = "Página " + PageN + " de ";
+            float len = bf.GetWidthPoint(text, 8);
+            Rectangle pageSize = document.PageSize;
+            cb.SetRgbColorFill(100, 100, 100);
+            cb.BeginText();
+            cb.SetFontAndSize(bf, 8);
+            cb.SetTextMatrix(pageSize.GetLeft(40), pageSize.GetBottom(30));
+            cb.ShowText(text);
+            cb.EndText();
+            cb.AddTemplate(template, pageSize.GetLeft(40) + len, pageSize.GetBottom(30));
+
+            cb.BeginText();
+            cb.SetFontAndSize(bf, 8);
+            cb.ShowTextAligned(PdfContentByte.ALIGN_RIGHT,
+                "Impreso en: " + PrintTime.ToString("dd/MM/yyyy hh:mm:ss"),
+                pageSize.GetRight(40),
+                pageSize.GetBottom(30), 0);
+            cb.EndText();
+        }
+
+        public override void OnCloseDocument(PdfWriter writer, Document document)
+        {
+            base.OnCloseDocument(writer, document);
+            template.BeginText();
+            template.SetFontAndSize(bf, 8);
+            template.SetTextMatrix(0, 0);
+            template.ShowText("" + (writer.PageNumber - 1));
+            template.EndText();
+        }
+
+    }
+}
--- Controllers/ReportesController.cs
+++ Controllers/ReportesController.cs
@@ -31,50 +31,69 @@ namespace ASGARDAPI.Controllers
             return View();
         }
 
-        //Creación de reporte
         [HttpGet]
         [Route("api/Reporte/reporte")]
         public async Task<IActionResult> reporte()
         {
-
             Document doc = new Document(PageSize.Letter);
-            FileStream file = new FileStream("hola mundo.pdf", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            PdfWriter writer = PdfWriter.GetInstance(doc, file);
+            doc.SetMargins(40f, 40f, 40f, 40f);
+            MemoryStream ms = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(doc, ms);
+
+            //Instanciamos la clase para el paginado y la fecha de impresión
+            var pe = new PageEventHelper();
+            writer.PageEvent = pe;
+
             doc.AddAuthor("Asgard");
             doc.AddTitle("Reporte PDF");
             doc.Open();
+
+            //Inicia cuerpo del reporte
 
             //Estilo y fuente personalizada
             BaseFont fuente = BaseFont.CreateFont(BaseFont.COURIER, BaseFont.CP1250, true);
             iTextSharp.text.Font parrafo = new iTextSharp.text.Font(fuente, 13f, iTextSharp.text.Font.NORMAL, new BaseColor(0, 0, 0));
             BaseFont fuente2 = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1250, true);
-            iTextSharp.text.Font parrafo2 = new iTextSharp.text.Font(fuente2, 10f, iTextSharp.text.Font.NORMAL, new BaseColor(0, 0, 0));
+            iTextSharp.text.Font parrafo2 = new iTextSharp.text.Font(fuente2, 11f, iTextSharp.text.Font.NORMAL, new BaseColor(0, 0, 0));
             BaseFont fuente3 = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1250, true);
             iTextSharp.text.Font parrafo3 = new iTextSharp.text.Font(fuente3, 15f, iTextSharp.text.Font.NORMAL, new BaseColor(0, 0, 0));
             BaseFont fuente4 = BaseFont.CreateFont(BaseFont.COURIER, BaseFont.CP1250, true);
-            iTextSharp.text.Font parrafo4 = new iTextSharp.text.Font(fuente, 11f, iTextSharp.text.Font.NORMAL, new BaseColor(0, 0, 0));
+            iTextSharp.text.Font parrafo4 = new iTextSharp.text.Font(fuente4, 11f, iTextSharp.text.Font.NORMAL, new BaseColor(0, 0, 0));
+
+            //Para las celdas
+            BaseFont fuente5 = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, true);
+            iTextSharp.text.Font parrafo5 = new iTextSharp.text.Font(fuente5, 10f, iTextSharp.text.Font.NORMAL, new BaseColor(0, 0, 0));
+
 
             //Encabezado
-            doc.Add(new Paragraph("ASOCIACIÓN COOPERTIVA DE APROVICIONAMIENTO AGROPECUARIO, AHORRO,", parrafo2) { Alignment = Element.ALIGN_CENTER });
-            doc.Add(new Paragraph("CRÉDITO Y CONSUMO DE SAN SEBASTIÁN DE RESPONSABILIDAD LIMITADA", parrafo2) { Alignment = Element.ALIGN_CENTER });
-            doc.Add(new Paragraph("ACAASS DE R.L.", parrafo3) { Alignment = Element.ALIGN_CENTER });
+            using (BDAcaassAFContext bd = new BDAcaassAFContext())
+            {
+                CooperativaAF oCooperativaAF = new CooperativaAF();
+                Cooperativa oCooperativa = bd.Cooperativa.Where(p => p.IdCooperativa == 1).First();
+                oCooperativaAF.idcooperativa = oCooperativa.IdCooperativa;
+                oCooperativaAF.nombre = oCooperativa.Nombre;
+                oCooperativaAF.descripcion = oCooperativa.Descripcion;
+                oCooperativaAF.logo = oCooperativa.Logo;
+
+                //Se agrega el encabezado
+                doc.Add(new Paragraph(oCooperativa.Descripcion, parrafo2) { Alignment = Element.ALIGN_CENTER });
+                doc.Add(new Paragraph(oCooperativa.Nombre, parrafo3) { Alignment = Element.ALIGN_CENTER });
+            }
 
             //Línea separadora
             Chunk linea = new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(1f, 100f, BaseColor.Black, Element.ALIGN_CENTER, 1f));
             doc.Add(linea);
             doc.Add(new Paragraph("REPORTE DE ÁREAS DE NEGOCIO", parrafo) { Alignment = Element.ALIGN_CENTER });
 
-            //Agregamos la fecha y la hora del reporte
-            doc.Add(new Paragraph(DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), parrafo4) { Alignment = Element.ALIGN_CENTER });
             //Espacio en blanco
             doc.Add(Chunk.Newline);
 
             //Agregamos una tabla
             var tbl = new PdfPTable(new float[] { 30f, 45f, 50f, 25f }) { WidthPercentage = 100f };
-            var c1 = new PdfPCell(new Phrase("CORRELATIVO"));
-            var c2 = new PdfPCell(new Phrase("NOMBRE DE ÁREA"));
-            var c3 = new PdfPCell(new Phrase("NOMBRE DE SUCURSAL"));
-            var c4 = new PdfPCell(new Phrase("UBICACIÓN"));
+            var c1 = new PdfPCell(new Phrase("CORRELATIVO", parrafo2));
+            var c2 = new PdfPCell(new Phrase("NOMBRE DE ÁREA", parrafo2));
+            var c3 = new PdfPCell(new Phrase("NOMBRE DE SUCURSAL", parrafo2));
+            var c4 = new PdfPCell(new Phrase("UBICACIÓN", parrafo2));
             //Agregamos a la tabla las celdas
             tbl.AddCell(c1);
             tbl.AddCell(c2);
@@ -84,24 +103,6 @@ namespace ASGARDAPI.Controllers
             //Extraemos de la base y llenamos las celdas
             using (BDAcaassAFContext bd = new BDAcaassAFContext())
             {
-                //Logo
-                //     CooperativaAF oCooperativaAF = new CooperativaAF();
-                //   Cooperativa oCooperativa = bd.Cooperativa.Where(p => p.IdCooperativa == 2).First();
-                //   oCooperativaAF.idcooperativa = oCooperativa.IdCooperativa;
-                //   oCooperativaAF.logo = oCooperativa.Logo;
-
-                //   byte[] _imagen_bytes = ctx.Tabla.Select(x => x.imagen).First();
-
-                //  using (var ms = new MemoryStream(_imagen_bytes))
-                //  {
-                //  Image returnImage = Image.FromStream(ms);
-                // }
-
-
-                //  iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(oCooperativa.Logo);
-                //  logo.ScaleAbsolute(75, 75);
-                //  doc.Add(logo);
-
                 IEnumerable<AreasDeNegocioAF> lista = (from area in bd.AreaDeNegocio
                                                        join sucursal in bd.Sucursal
                                                         on area.IdSucursal equals sucursal.IdSucursal
@@ -117,10 +118,10 @@ namespace ASGARDAPI.Controllers
                                                        }).ToList();
                 foreach (var area in lista)
                 {
-                    c1.Phrase = new Phrase(area.Correlativo);
-                    c2.Phrase = new Phrase(area.Nombre);
-                    c3.Phrase = new Phrase(area.nombreSucursal);
-                    c4.Phrase = new Phrase(area.ubicacion);
+                    c1.Phrase = new Phrase(area.Correlativo, parrafo5);
+                    c2.Phrase = new Phrase(area.Nombre, parrafo5);
+                    c3.Phrase = new Phrase(area.nombreSucursal, parrafo5);
+                    c4.Phrase = new Phrase(area.ubicacion, parrafo5);
                     //Agregamos a la tabla
                     tbl.AddCell(c1);
                     tbl.AddCell(c2);
@@ -130,17 +131,13 @@ namespace ASGARDAPI.Controllers
             }
             doc.Add(tbl);
 
-            //YA FUNCIONA EL env.WebRootPath, solo crea otro metodo y proba solo la imagen 
-            // iTextSharp.text.Image logo=iTextSharp.text.Image.GetInstance(Path.Combine(env.WebRootPath))
-
             writer.Close();
             doc.Close();
-            file.Dispose();
-            var pdf = new FileStream("hola mundo.pdf", FileMode.Open, FileAccess.Read);
-            return File(pdf, "application/pdf");
-
+            ms.Seek(0, SeekOrigin.Begin);
+            return File(ms, "application/pdf");
 
         }
+        
 
     }
 }
