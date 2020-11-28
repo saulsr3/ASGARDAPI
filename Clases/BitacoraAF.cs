1e59450b1c543d1bb849b7394fa7a55105d35410
Kevin Jovel <kevinjovel9@gmail.com>
2020-11-28 11:35:19 -0600

    bitacora

---
+++ Clases/BitacoraAF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASGARDAPI.Clases
+{
+    public class BitacoraAF
+    {
+        public int idUsuario { get; set; }
+        public string fecha { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ Controllers/BitacoraController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASGARDAPI.Clases;
+using ASGARDAPI.Models;
+
+namespace ASGARDAPI.Controllers
+{
+    public class BitacoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("api/Bitacora/guardarTransaccion/{idUsuario}/{descripcion}")]
+        public int guardarTransaccion(int idUsuario,string descripcion)
+        {
+            int rpta = 0;
+            try
+            {
+                using (BDAcaassAFContext bd = new BDAcaassAFContext())
+                {
+                    Bitacora oBitacora = new Bitacora();
+                    oBitacora.IdUsuario = idUsuario;
+                    oBitacora.Descripcion = descripcion;
+                    oBitacora.Fecha = DateTime.Now;
+                    bd.Bitacora.Add(oBitacora);
+                    bd.SaveChanges();
+                    rpta = 1;
+                        
+                }
+            }
+
+            catch (Exception ex)
+            {
+                rpta = 0;
+            }
+            return rpta;
+        }
+    }
+}
