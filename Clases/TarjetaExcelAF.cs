66515c0493539bcf3e17ec7fa8957b480820aa85
kevRei08 <kevin_rey09@hotmail.com>
2020-12-02 16:05:26 -0600

    Método para datos en archivo excel

---
+++ Clases/TarjetaExcelAF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ASGARDAPI.Clases
+{
+    public class TarjetaExcelAF
+    {
+        //Variables
+        public string codigo { get; set; }
+        public string fecha { get; set; }
+        public string concepto { get; set; }
+        public double montoTransaccion { get; set; }
+        public double depreciacionAnual { get; set; }
+        public double depreciacionAcumulada { get; set; }
+        public double valorActual { get; set; }
+        public double valorTransaccion { get; set; }
+        public string descripcion { get; set; }
+    }
+}
