f7b2abe36aadc5d92093e756e420a0ebd3262c79
Kevin Jovel <kevinjovel9@gmail.com>
2021-01-15 09:30:32 -0600

    connectionstring

--- Models/BDAcaassAFContext.cs
+++ Models/BDAcaassAFContext.cs
@@ -47,10 +47,8 @@ namespace ASGARDAPI.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBAcaassAF;database=BDAcaassAF;Integrated Security=true");
-                 optionsBuilder.UseSqlServer("Data Source=tcp:server-db-asgard.database.windows.net,1433;Initial Catalog=DBAcaassAF;User Id=adminAsgard@server-db-asgard;Password=Root14003$");
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBAcaassAF;database=BDAcaassAF;Integrated Security=true");
             }
-        }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
